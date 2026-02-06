4bb4d0bdd8548bcdb60e2cdc5c52f514c1aeedff
libguim <libguim@gmail.com>
2026-02-06 17:36:32 +0900

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,79 @@
+import { useSearchParams } from "react-router-dom";
+import Button from "../component/Button";
+import Header from "../component/Header";
+import Editor from "../component/Editor";
+import {useState, useContext, useEffect} from "react";
+import { DiaryStateContext } from "../App";
+import { getMonthRangeByDate } from "../util";
+import DiaryList from "../component/DiaryList";
+
+const Home = () => {
+    // const [searchParams, setSearchParams] = useSearchParams();
+    // console.log(searchParams.get("sort"));
+    const data = useContext(DiaryStateContext);
+    const [pivotDate, setPivotDate] = useState(new Date());
+    const [filteredData, setFilteredData] = useState([]);
+    const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
+
+    const onIncreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+    };
+    const onDecreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+    };
+
+    useEffect(() => {
+        if(data.length >= 1){
+            const {beginTimeStamp, endTimeStamp} = getMonthRangeByDate(pivotDate);
+            setFilteredData(
+                data.filter(
+                    (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp
+                )
+            )
+        }else{
+            setFilteredData([]);
+        }
+    }, [data, pivotDate]);
+
+    return (
+        <div>
+            <Header 
+                title={"2022년 n월"}
+                leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+                rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
+            />
+            <DiaryList data={filteredData} />            
+            <Editor 
+                initData={{
+                    date: new Date().getTime(),
+                    emotionId: 1,
+                    content: "이전에 작성했던 일기",
+                }}
+                onSubmit={() => alert("작성 완료!")}
+            />
+            {/* <Header 
+                title={"Home"}
+                leftChild={
+                    <Button 
+                    type="positive"
+                    text={"긍정 버튼"}
+                    onClick={() => {
+                        alert("긍정 버튼");
+                    }}
+                    />
+                }
+                rightChild={
+                    <Button 
+                        type="negative"                    
+                        text={"부정 버튼"}
+                        onClick={() => {
+                            alert("부정 버튼");
+                        }}
+                    />
+                }
+            /> */}
+        </div>
+    );
+};
+
+export default Home;
